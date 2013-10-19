146c976a835b8b03a23af8dd43d56b79f7eac708
Rebecca Lynch <becalyna@gmail.com>
2013-10-19 12:56:11 -0400

    Initial Commit

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+// JavaScript Document
+
+jQuery(function($) {
+	'use strict';
+	
+	$('document').ready(function() {
+		$.support.cors = true;
+		
+		$(document).bind('mobileinit', function() {
+			$.mobile.allowCrossDomainPages = true;
+		});
+		
+		
+		ListApp.init();
+		document.addEventListener('deviceready', onDeviceReady, true);
+	})
+	
+	function onDeviceReady(){
+		ListApp.init();
+	}
+})
